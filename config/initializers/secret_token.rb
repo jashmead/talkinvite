b31bc0d989064add868be86d4a694413e8832750
jashmead <john.ashmead@ashmeadsoftware.com>
2013-08-23 17:49:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talkinvite::Application.config.secret_key_base = '73e99ec03a3bc7ece0097ef55f4ccafefb6d533c719fd54a971e580cd447e8846e4029fa86d6585e8dfa8394eac98fc6b727bfb0459fbfcf443f8b6f0ba931e3'
