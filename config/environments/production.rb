e0af5cf58663cf613bac6827979cc0dd56eb0d6f
jashmead <john.ashmead@ashmeadsoftware.com>
2013-08-24 16:09:37 -0400

    turned on assets pipeline, marked user_user, user_talk, and talk_talk as not needed for MVP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Talkinvite::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # 8/24/13 -- set to true for heroku
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
