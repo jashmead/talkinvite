fd3aa2eb2e9616b06bba5759d619b54a15976f53
jashmead <john.ashmead@ashmeadsoftware.com>
2013-09-01 15:55:56 -0400

    added some debugging stuff, in prep for going to 8.2.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def sign_in(person)
     # normal format of cookies is cookies[:key] = { value: value, expires: expire_date }
     #   but, if assigning to cookies.permanent, get expires in 20 years
@@ -6,12 +7,16 @@ module SessionsHelper
     # add a current person attribute to the current object (a controller)
     # this is really an invocation of the 'current_person=' attribute writer
     self.current_person = person
+    logger.debug("ZZ: Sign in:  #{person.inspect}") #DDT
+    logger.debug("ZZ: Flash:  #{flash.inspect}") #DDT
   end
+
   # why is this squib needed?
   def current_person=(person)
     # stores variable for later use
     @current_person = person
   end
+
   # this won't work! apparently doesn't carry thru to next page
   ##  def current_person
   ##    @current_person
@@ -21,4 +26,5 @@ module SessionsHelper
     # we could also use Person.find_by_remember_token( cookies[:remember_token] )
     @current_person ||= Person.find_by( :remember_token,cookies[:remember_token] )
   end
+
 end
