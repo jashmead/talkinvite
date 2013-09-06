56f3b49b72220d3fec582751b80f7940b13bec5f
jashmead <john.ashmead@ashmeadsoftware.com>
2013-09-06 17:34:20 -0400

    section 8.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,17 @@ module SessionsHelper
 
   def sign_in(person)
     logger.debug("Sign in: person: #{person.inspect}") #DDT
+
+    remember_token = Person.new_remember_token
+
     # normal format of cookies is cookies[:key] = { value: value, expires: expire_date }
     #   but, if assigning to cookies.permanent, get expires in 20 years
-    cookies.permanent[:remember_token] = person.remember_token  
+    #   put unencrypted form of remember_token in the person's cookie:
+    cookies.permanent[:remember_token] = remember_token  
+
+    #   save encrypted form to the database record:
+    person.update_attribute[:remember_token, Person.encrypt(remember_token)]
+
     # add a current person attribute to the current object (a controller)
     # this is really an invocation of the 'current_person=' attribute writer
     self.current_person = person
@@ -20,6 +28,7 @@ module SessionsHelper
   # why is this squib needed?
   def current_person=(person)
     # stores variable for later use
+    # any reason for @current_person versus self.current_person?
     @current_person = person
   end
 
@@ -30,8 +39,15 @@ module SessionsHelper
   # instead use:
   def current_person
     logger.debug("current_person: current_person: #{@current_person.inspect}") # DDT
-    # the following syntax fails with a PostgreSQL error!
-    # @current_person ||= Person.find_by( :remember_token, cookies[:remember_token] )
+
+    remember_token = Person.encrypt(cookies[:remember_token])
+
+    # the following syntax fails with a PostgreSQL error
+    # @current_person ||= Person.find_by( :remember_token, remember_token )
+    ## PG::InvalidTextRepresentation: ERROR:  invalid input syntax for type boolean: "remember_token"
+    ## LINE 1: SELECT  "people".* FROM "people"  WHERE ('remember_token') L...
+    ## : SELECT  "people".* FROM "people"  WHERE ('remember_token') LIMIT 1
+
     # have to spell out the find_by_remember_token
     @current_person ||= Person.find_by_remember_token( cookies[:remember_token] )
   end
