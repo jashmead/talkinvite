d0a073190fe30ef96c65bfc4507006dc062b2bfb
jashmead <john.ashmead@ashmeadsoftware.com>
2013-10-02 12:08:18 -0400

    setup tests for talks/search

--- spec/requests/search_pages_spec.rb
+++ spec/requests/search_pages_spec.rb
@@ -10,12 +10,13 @@ describe "search" do
 
   describe "search/people" do
 
-    ## let has to go here & not in a 'before' group?
+    ## let has to go here & not in a 'before' group? apparently so
     let(:person) { FactoryGirl.create(:person) }
 
     describe "simple search form" do
 
       before do
+        ## and it looks as if visits have to go in a before group
         visit search_people_path
       end
 
@@ -32,6 +33,7 @@ describe "search" do
           expect(page).to have_content("No matching people found")
           expect(page).to have_title("Search for people")
         end
+
       end
 
       describe "search with one row expected" do
@@ -68,6 +70,79 @@ describe "search" do
             expect(page).to have_content('Found 2 matching people')
             expect(page).to have_title("People")
             expect(page).to_not have_title("Search for people")
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+  ## there are probably some DRY opportunities here, especially once we add 'start' to the mix
+  describe "search/talks" do
+
+    ## let has to go here & not in a 'before' group? apparently so
+    let(:talk) { FactoryGirl.create(:talk) } 
+
+    describe "simple search form" do
+
+      before do
+        ## and it looks as if visits have to go in a before group
+        visit search_talks_path
+      end
+
+      describe "search with no rows expected" do
+
+        before do
+          ## fill_in has to be in 'before' section?
+          fill_in "search_q",    with: "ZZTop"
+          click_button "Search"
+        end
+
+        it "should return to the search page with a message" do
+          ## would like a more accurate response, specifically that it was an error message we looked at
+          expect(page).to have_content("No matching talks found")
+          expect(page).to have_title("Search for talks")
+        end
+
+      end
+
+      describe "search with one row expected" do
+
+        before do
+          fill_in "search_q",    with: talk.summary
+          click_button "Search"
+        end
+
+        describe "after click of search" do
+
+          it "should render the found page" do
+            expect(page).to have_content('Found one matching talk')
+            expect(page).to have_title("Talk")
+            expect(page).to_not have_title("Search for talks")
+          end
+
+        end
+
+      end
+
+      describe "search with two rows expected" do
+
+        before do
+          FactoryGirl.create(:talk)
+          FactoryGirl.create(:talk)
+          fill_in "search_q",    with: "About"
+          click_button "Search"
+        end
+
+        describe "after click of search" do
+
+          it "should render the found page" do
+            expect(page).to have_content('Found 2 matching talks')
+            expect(page).to have_title("Talks")
+            expect(page).to_not have_title("Search for talks")
           end
 
         end
---
+++ spec/views/talks/search.html.erb_spec.rb
@@ -0,0 +1,33 @@
+## talks/search
+##  elements expected:
+##    page itself (visit & check title)
+##    a search form:
+##      search fields
+##      button saying search
+
+require 'spec_helper'
+
+describe "talks/search" do
+
+  subject { page }        ## what, exactly, does this do? sets 'it', I think
+
+  let(:page_title) { 'Search for talks' }
+  let(:heading) { 'Search for talks' } # can't use :heading here?
+
+  describe "renders the search talk form" do
+    before { visit "/people/talks" }  ## replace with a 'path', once we have worked that out
+
+    ## note each of these tests restarts the whole process, nice
+    it { should have_title(page_title) }
+
+    it { should have_selector('h1', text: heading) }
+
+    ## is 'person_search' correct?
+    ## is value => '' correct?
+    ## it { should have_field "person_search", :type => :search, :value => '' }
+
+    it { should have_button 'Search' }
+
+  end
+
+end
