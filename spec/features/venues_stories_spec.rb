678f809169060672ca0aac22947f8362ac1a4612
jashmead <john.ashmead@ashmeadsoftware.com>
2013-10-15 16:42:57 -0400

    added some pending tests for venues

---
+++ spec/features/venues_stories_spec.rb
@@ -0,0 +1,18 @@
+## venue_stories_spec -- check accept/decline/maybe
+
+describe "check venue stories" do
+
+  let(:person) { FactoryGirl.create(:person) }
+  let(:venue) { FactoryGirl.create(:venue, :person => person ) }
+
+  pending "add pure venue"
+
+  pending "add venue for person"
+
+  pending "add venue for talk"
+
+  pending "map venue"
+
+  pending "find venues near a location"
+
+end
