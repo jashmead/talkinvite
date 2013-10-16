d526f5ec821b9e6bd5c3491de6826f210f3808fc
jashmead <john.ashmead@ashmeadsoftware.com>
2013-10-15 23:39:03 -0400

    fixed another messages fixup

--- db/migrate/20131016011047_fixup_messages.rb
+++ db/migrate/20131016011047_fixup_messages.rb
@@ -1,7 +1,7 @@
 class FixupMessages < ActiveRecord::Migration
   def self.up
-    change_column :messages, :talk_id, :integer, { :null => nil }
-    execute("alter table messages drop constraint message2talk_fk")
-    execute("alter table messages add constraint message2talk_fk foreign key (talk_id) references talks (id) on delete cascade on update cascade")
+    ## change_column :messages, :talk_id, :integer, { :null => nil }
+    ## execute("alter table messages drop constraint message2talk_fk")
+    ## execute("alter table messages add constraint message2talk_fk foreign key (talk_id) references talks (id) on delete cascade on update cascade")
   end
 end
