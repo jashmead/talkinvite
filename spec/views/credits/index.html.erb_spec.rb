d7a99bd3e93ad731db3e170655b05b1814847664
jashmead <john.ashmead@ashmeadsoftware.com>
2013-10-23 09:05:23 -0400

    tests working on index for credits

--- spec/views/credits/index.html.erb_spec.rb
+++ spec/views/credits/index.html.erb_spec.rb
@@ -4,12 +4,12 @@ describe "credits/index" do
   before(:each) do
     assign(:credits, [
       stub_model(Credit,
-        :name => "MyWorthiness",
-        :description => "TheServiceSupplied"
+        :name => "Lad A",
+        :description => "Service A"
       ),
       stub_model(Credit,
-        :name => "MyWorthiness",
-        :description => "TheServiceSupplied"
+        :name => "Lass B",
+        :description => "Service B"
       )
     ])
   end
@@ -17,7 +17,14 @@ describe "credits/index" do
   it "renders a list of credits" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyWorthiness".to_s, :count => 2
-    assert_select "tr>td", :text => "TheServiceSupplied".to_s, :count => 2
+    assert_select "li", :text => /Lad A/, count: 1
+    assert_select "li",:text => /Lass B/, count: 1
   end
+
+  # what is test for admin? in spec arena?
+  it "only shows new button if you are an admin" do
+    pending "No New if not admin"
+    pending "New if admin"
+  end
+
 end
