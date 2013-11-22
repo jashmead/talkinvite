f9e6352a19a0376ff759ea4e97dcae064bd57d0b
jashmead <john.ashmead@ashmeadsoftware.com>
2013-11-22 12:25:18 -0500

    cleanup on person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,49 +8,56 @@
 # 1.  remember_token -- sent to user as part of a cookie, then used to find him/her
 # 1.  password, password_confirmation, password_digest
 # 1.  description -- text, optional
+#
+# TBD: instantiate admin flag
+# TBD: instantiate sub flag -- tho not really needed till Phase III, could just get rid of
 # 
 # == Children
-# 1. Comments
-# 1. Members
-# 1. Messages -- from sender & from receiver
+
+# 1. Talks
+# 1. Services
 # 1. Posts
-# 1. Talks -- creator
+# 1. Members
+# 1. Comments
+# 1. Messages -- from sender & from receiver
+# 1. Calendars
+# 1. Maps
 
 # == Fields contemplated
 # 1. active_flag -- use to deactivate when the user has killed, in case there is other data we need to keep associated with this
 # 1. current_talk_id -- store that id in the database
-# 1. url
-# 1. photo
-# 1. preferred start page (currently defaulting to sitemap)
+# 1. url -- bad for security reasons
+# 1. photo -- see attachments
+# 1. preferred start page (currently defaulting to sitemap), can store in session as last page
 
 class Person < ActiveRecord::Base
 
   ## this line *creates* the attribute/method of Person called 'talks'
   has_many :talks, inverse_of: :person, dependent: :destroy
 
+  has_many :services, inverse_of: :person, dependent: :destroy
+
+  has_many :posts, inverse_of: :person, dependent: :destroy
+
+  has_many :members, inverse_of: :person, dependent: :destroy
+  has_many :memberships, inverse_of: :person, through: :members, source: :talks
+
+  has_many :comments, inverse_of: :person, dependent: :destroy
+
   has_many :sent_messages, inverse_of: :person, foreign_key: "sender_id", class_name: "Message", dependent: :destroy
   has_many :receivers, inverse_of: :person, through: :sent_messages, source: :receivers, dependent: :destroy
   has_many :received_messages, inverse_of: :person, foreign_key: "receiver_id", class_name: "Message", dependent: :destroy
   has_many :senders, inverse_of: :person, through: :received_messages, source: :sender
 
-  has_many :members, inverse_of: :person, dependent: :destroy
-  has_many :memberships, inverse_of: :person, through: :members, source: :talks
-
-  has_many :posts, inverse_of: :person, dependent: :destroy
-
-  has_many :comments, inverse_of: :person, dependent: :destroy
-
-  has_many :services, inverse_of: :person, dependent: :destroy
-
   has_many :maps, inverse_of: :person, dependent: :destroy
   has_many :calendars, inverse_of: :person, dependent: :destroy
+
+  before_create :create_remember_token
 
   before_save do
     # TBD:  make the name lowercase & space-free? -- would rather not, tho would let us route to /people/anonymous/talks...
     self.email = email.downcase
   end
-
-  before_create :create_remember_token
 
   after_save :create_talkinvite_service
 
@@ -65,22 +72,15 @@ class Person < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: true
 
-  # TBD:  replace validates password & confirmation with a validate block, called on new account & from reset forms, but not from settings
-  #   -- or, use update attribute or related to do the save
-  #   -- or, add in an unless { controller.action_name === 'settings' }
-  #   -- or, use 'with_options'
+  # TBD: we want to validate password on new account & change password but not on settings, how?
+  #   -- conditional validation possible, but may open up security holes
+  #   -- also, unclear what the trigger for such should be
   has_secure_password
-  logger.debug(__FILE__ + ": @action_name: #{@action_name}, self: #{self.inspect}")
-  # TBD: we want to validate password on new account & change password but not on settings
-  #   -- how?
-  # if false 
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  # end
 
-  # TBD: there is a problem with find_by(:remember_token, remember_token); why?
+  # TBD: there is a problem with find_by(:remember_token, remember_token): suspect presence of '_' inside field name
   def find_by_remember_token( encrypted_remember_token ) 
-    ##  logger.debug("Person.find_by_remember_token: encrypted_remember_token: %{encrypted_remember_token}") #DDT
     person = Person.where(remember_token: encrypted_remember_token)
   end
 
@@ -93,17 +93,15 @@ class Person < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)  # 'to_s' is to make sure we can handle nil tokens
   end
 
-  # tried 'anonymous' in the app/helpers, got whined at...
+  # TBD: why can't anonymous be defined in the helpers?
   def self.anonymous 
     begin 
-      # logger.debug("Person.anonymous")
       self.find_by_name!('anonymous')
     end
   end
 
   # TBD:  add in create of default service?
   def self.force!(name, email, password, options = {})
-    # logger.debug("Person.force!: #{name}, #{email}, #{password}, #{options.inspect}")
     Person.find_by_name( name ) or
       Person.create!(
         name: name,
@@ -126,6 +124,7 @@ class Person < ActiveRecord::Base
     def create_talkinvite_service
       # logger.debug("Person.create_talkinvite_service: self: #{self.inspect}")
 
+      # TBD: shift from id to new_record?
       if self.id?
         # logger.debug("Person.create_talkinvite_service: self.id: #{self.id.inspect}")
         @services = Service.where('person_id = ? and service_type = ?', self.id, 'talkinvite')
