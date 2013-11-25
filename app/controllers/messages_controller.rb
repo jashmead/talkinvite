36fa28d87ff179ca8f12339136d1d5bc793e1f38
jashmead <john.ashmead@ashmeadsoftware.com>
2013-11-25 09:02:43 -0500

    still more cleanup on sitemap

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -68,6 +68,7 @@ class MessagesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
+      # TBD:  verify that either user is admin or the message is from/to the current user
       MessagesController.current_message = @message = Message.find(params[:id])
     end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -45,46 +45,73 @@ class StaticPagesController < ApplicationController
     # TBD:  add a current_message element in?
     
     # note: the paths have to be local to the sitemap method; making the routes variables class variables failed.
-    admin_routes = [
-      [ new_help_path, 'New Help' , 'helps' ],
-      [ new_faq_path, 'New FAQ' , 'faqs' ],
-      [ new_credit_path, 'New Credit' , 'credits' ]
-    ]
-    base_routes = [
-      [ search_path, 'Search for Talks', 'talks' ],
-      [ helps_path, 'List of Help Pages' , 'helps' ],
-      [ faqs_path, 'FAQs' , 'faqs' ],
-      [ credits_path, 'Credits' , 'credits' ],
-      [ static_pages_about_path, 'About' , 'static_pages' ],
-      [ static_pages_contact_path, 'Contact' , 'static_pages' ],
-      [ static_pages_privacy_path, 'Privacy' , 'static_pages' ],
-      [ static_pages_sitemap_path, 'Site Map', 'static_pages' ]
-    ]
-    current_talk_routes = [ [ control_talk_path(TalksController.current_talk), "Current Talk", 'talks' ] ]
-    signed_in_routes = [
-      [ home_path, 'Home', 'people' ],
-      [ settings_path, 'Settings', 'people' ],
-      [ profile_path, 'Profile', 'people' ],
-      [ change_password_path, 'Change Password', 'people' ],
-      [ new_talk_path, 'Create Talk', 'talks' ],
-      [ my_messages_path, 'My Messages', 'messages' ],
-      [ search_people_path, 'Search for People', 'people' ]
-    ]
-    current_message_routes = [ [ message_path(MessagesController.current_message), "Current Message", 'messages' ] ]
-    not_signed_in_routes = [
-      [ signin_path, 'Sign In', 'sessions' ],
-      [ signup_path, 'New Account', 'people' ],
-      [ reset_password_path, 'Reset Password', 'sessions' ]
-    ]
+    # this means they have to be computed; to keep this proc simple, have pushed the computes into private methods, see below
 
-    @routes = signed_in? ? signed_in_routes : not_signed_in_routes
-
-    @routes += current_talk_routes if TalksController.current_talk
-    
-    @routes += admin_routes if admin?
-
-    @routes += base_routes
+    @routes = user_routes + talk_routes + message_routes + admin_routes + common_routes
 
   end
+
+  private
+
+    def admin_routes
+      if admin?
+        [
+          [ new_help_path, 'New Help' , 'helps' ],
+          [ new_faq_path, 'New FAQ' , 'faqs' ],
+          [ new_credit_path, 'New Credit' , 'credits' ]
+        ]
+      else
+        []
+      end
+    end
+
+    def common_routes
+      [
+        [ search_path, 'Search for Talks', 'talks' ],
+        [ helps_path, 'List of Help Pages' , 'helps' ],
+        [ faqs_path, 'FAQs' , 'faqs' ],
+        [ credits_path, 'Credits' , 'credits' ],
+        [ static_pages_about_path, 'About' , 'static_pages' ],
+        [ static_pages_contact_path, 'Contact' , 'static_pages' ],
+        [ static_pages_privacy_path, 'Privacy' , 'static_pages' ],
+        [ static_pages_sitemap_path, 'Site Map', 'static_pages' ]
+      ]
+    end
+  
+    def message_routes 
+      if MessagesController.current_message
+        [ [ message_path(MessagesController.current_message), "Current Message", 'messages' ] ]
+      else
+        []
+      end
+    end
+        
+    def talk_routes
+      if TalksController.current_talk
+        [ [ control_talk_path(TalksController.current_talk), "Current Talk", 'talks' ] ]
+      else
+        []
+      end
+    end
+
+    def user_routes
+      if signed_in? 
+        [
+          [ home_path, 'Home', 'people' ],
+          [ settings_path, 'Settings', 'people' ],
+          [ profile_path, 'Profile', 'people' ],
+          [ change_password_path, 'Change Password', 'people' ],
+          [ new_talk_path, 'Create Talk', 'talks' ],
+          [ my_messages_path, 'My Messages', 'messages' ],
+          [ search_people_path, 'Search for People', 'people' ]
+        ]
+      else
+        [
+          [ signin_path, 'Sign In', 'sessions' ],
+          [ signup_path, 'New Account', 'people' ],
+          [ reset_password_path, 'Reset Password', 'sessions' ]
+        ]
+      end
+    end
 
 end
